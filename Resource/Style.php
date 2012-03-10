270b3ae1349706d72d8a1fe48c678c19abbf6e56
padigan <ben.riley@gmail.com>
2012-03-10 23:39:53 +0000

    recommit

--- Resource/Style.php
+++ Resource/Style.php
@@ -1,71 +1,10 @@
 <?php
 /**
- * XeroPoint Style resource class
+ * XeroPoint style resource class
  * 
  * @author Ben Riley <ben.riley@gmail.com>
  */
 
-abstract class XeroPoint_Resource_Style extends XeroPoint_Resource_Abstract {
-	
-	/**
-	 * returns the complete output including headers to the client
-	 * 
-	 * @param bool $useCaching
-	 * @param bool $useGZIP
-	 */
-	public function sendClientResponse($useCaching = true, $useGZIP = true) {
-		// first start buffering as normal
-		ob_end_clean ();
-		ob_start ();
-		
-		// now output all code
-		$this->output ();
-		
-		// capture the contents above and remove any <style> tags if present
-		$bufferOutput = str_replace ( array ( 
-			'<style>', 
-			'</style>' 
-		), array ( 
-			'', 
-			'' 
-		), ob_get_clean () );
-		
-		// content type
-		header ( 'Content-Type: text/css' );
-		
-		// using caching?
-		if ($useCaching) {
-			header ( 'Cache-Control: max-age=2592000, public' );
-			header ( 'Expires-Active: On' );
-			header ( 'Expires: Fri, 1 Jan 2500 01:01:01 GMT' );
-			header ( 'Pragma:' );
-		}
-		
-		// using GZIP?
-		if ($useGZIP) {
-			header ( 'Content-Encoding: gzip' );
-			
-			if (! ob_start ( 'ob_gzhandler' )) {
-				echo 'resource buffering error';
-			}
-		} else {
-			// normal buffering
-			ob_start ();
-		}
-		
-		// send code
-		echo $bufferOutput;
-		
-		// send all to client
-		ob_flush ();
-		
-		// and just for good measure
-		exit ();
-	}
-	
-	/**
-	 * must implement this method to output your CSS
-	 * 
-	 */
-	abstract protected function output();
+class XeroPoint_Resource_Style extends XeroPoint_Resource_Abstract {
+
 }
