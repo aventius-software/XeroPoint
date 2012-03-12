1554458bab70ffa516bb10dbe882b5011beeee25
padigan <ben.riley@gmail.com>
2012-03-12 14:47:06 +0000

    added basic test style resource responder

---
+++ Resource/Responder/Style/Test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * test style class
+ * 
+ */
+
+class XeroPoint_Resource_Responder_Style_Test extends XeroPoint_Resource_Responder_Style {
+	
+	protected function output() {
+		echo 'test CSS output';
+	}
+}
