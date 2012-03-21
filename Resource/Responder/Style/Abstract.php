7e4d5a4bb5b1d943db45a745b5a15bcb88461663
padigan <ben.riley@gmail.com>
2012-03-21 22:05:11 +0000

    removed invalid reference to abstract buildResponse method

--- Resource/Responder/Style/Abstract.php
+++ Resource/Responder/Style/Abstract.php
@@ -5,65 +5,28 @@
  * @author Ben Riley <ben.riley@gmail.com>
  */
 
-abstract class XeroPoint_Resource_Responder_Style_Abstract implements XeroPoint_Interface_Resource_Responder {
+abstract class XeroPoint_Resource_Responder_Style_Abstract extends XeroPoint_Resource_Responder_Abstract {
 	
 	/**
-	 * returns the complete output including headers to the client
+	 * override the default pre-process method to parse the CSS response and send correct content type header
 	 * 
-	 * @param bool $useCaching
-	 * @param bool $useGZIP
+	 * @param string $response
+	 * @return string
 	 */
-	public function sendClientResponse($useCaching = true, $useGZIP = true) {
-		// start buffer capture
-		ob_start ();
-		
-		// now output all code
-		$this->output ();
-		
-		// capture the contents above and remove any <style> tags if present
-		$bufferOutput = str_replace ( array ( 
+	protected function preProcessResponse($response) {
+		// remove any style tags if present
+		$response = str_replace ( array ( 
 			'<style>', 
 			'</style>' 
 		), array ( 
 			'', 
 			'' 
-		), ob_get_clean () );
+		), $response );
 		
-		// content type
+		// remember to send the correct content type header! helps!
 		header ( 'Content-Type: text/css' );
 		
-		// using caching?
-		if ($useCaching) {
-			header ( 'Cache-Control: max-age=2592000, public' );
-			header ( 'Expires-Active: On' );
-			header ( 'Expires: Fri, 1 Jan 2500 01:01:01 GMT' );
-			header ( 'Pragma:' );
-		}
-		
-		// using GZIP?
-		if ($useGZIP) {
-			header ( 'Content-Encoding: gzip' );
-			
-			if (! ob_start ( 'ob_gzhandler' )) {
-				echo 'resource buffering error';
-			}
-		} else {
-			// normal buffering
-			ob_start ();
-		}
-		
-		// send code
-		echo $bufferOutput;
-		
-		// send all to client
-		ob_flush ();
-	}
-	
-	/**
-	 * must implement this method to output your CSS
-	 * 
-	 */
-	public function output() {
-		return;
+		// return processed response
+		return $response;
 	}
 }
