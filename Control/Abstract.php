f1ac487e2a2c77242dbf8c13520e2869cbb33f74
padigan <ben.riley@gmail.com>
2012-04-07 16:49:55 +0100

    first commit

---
+++ Control/Abstract.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * XeroPoint control abstract class
+ * 
+ * @author Ben Riley <ben.riley@gmail.com>
+ * @package XeroPoint
+ */
+
+abstract class XeroPoint_Control_Abstract {
+	
+	/**
+	 * GET method identifier
+	 * 
+	 * @var string
+	 */
+	const METHOD_GET = 'get';
+	
+	/**
+	 * POST method identifier
+	 * 
+	 * @var string
+	 */
+	const METHOD_POST = 'post';
+	
+	/**
+	 * holds the ID's of all declared controls in a single script
+	 * 
+	 * @var array
+	 */
+	protected static $controls = array ();
+	
+	/**
+	 * flag to indicate display or not for the controls label
+	 * 
+	 * @var bool
+	 */
+	protected $displayLabel = true;
+	
+	/**
+	 * the ID of this control
+	 * 
+	 * @var string
+	 */
+	protected $id;
+	
+	/**
+	 * contains the label text
+	 * 
+	 * @var string
+	 */
+	protected $label;
+	
+	/**
+	 * holds the max length in characters of a control value (default max of 255)
+	 * 
+	 * @var int
+	 */
+	protected $maxLength = 255;
+	
+	/**
+	 * holds the transmission method
+	 * 
+	 * @var string
+	 */
+	protected $method;
+	
+	/**
+	 * create a new control
+	 * 
+	 * @param string $id
+	 * @param string $method
+	 */
+	public function __construct($id, $method = self::METHOD_POST) {
+		// must specify a form name!
+		if (empty ( $id )) {
+			throw new Exception ( 'you must specify an id when creating a control' );
+		}
+		
+		// when performing tests reset the cache!
+		if (class_exists ( 'PHPUnit_Framework_TestCase' ) && array_key_exists ( $id, self::$controls )) {
+			unset ( self::$controls [$id] );
+		}
+		
+		// is this a duplicate form?
+		if (in_array ( $id, self::$controls )) {
+			throw new Exception ( 'this control is already defined elsewhere in this script' );
+		}
+		
+		// set the name of this form
+		$this->id = $id;
+		
+		// save this form
+		self::$controls [$id] = $id;
+		
+		// save the method type
+		$this->method = $method;
+	}
+	
+	/**
+	 * must implement this method to return string for the controls html
+	 * 
+	 * @return string
+	 */
+	abstract protected function getControlHtml();
+	
+	/**
+	 * returns the ID of this control
+	 * 
+	 * @return string
+	 */
+	public function getID() {
+		return $this->id;
+	}
+	
+	/**
+	 * returns this controls label html
+	 * note that if no label text has been set this method will return just a blank string and no tags at all
+	 * 
+	 * @return string
+	 */
+	public function getLabelHtml() {
+		return is_null ( $this->label ) ? '' : '<label id="' . $this->id . '_label" for="' . $this->id . '">' . htmlentities ( $this->label ) . '</label>';
+	}
+	
+	/**
+	 * returns the current max length of input value in characters for this control
+	 * 
+	 * @return int
+	 */
+	public function getMaxLength() {
+		return $this->maxLength;
+	}
+	
+	/**
+	 * returns the current method for use when collecting value for this control
+	 * 
+	 * @return string
+	 */
+	public function getMethod() {
+		return $this->method;
+	}
+	
+	/**
+	 * tells this control to not output a label
+	 * 
+	 * @return XeroPoint_Control_Abstract
+	 */
+	public function hideLabel() {
+		$this->displayLabel = false;
+		return $this;
+	}
+	
+	/**
+	 * set the text for this controls label
+	 * note that all html will be encoded within the label when it is finally rendered
+	 * 
+	 * @param string $label
+	 * @return XeroPoint_Control_Abstract
+	 */
+	public function setLabel($label) {
+		$this->label = $label;
+		return $this;
+	}
+	
+	/**
+	 * set the max length for this controls input value
+	 * 
+	 * @param int $maxLength
+	 * @return XeroPoint_Control_Abstract
+	 */
+	public function setMaxLength($maxLength) {
+		if ((is_int ( $maxLength ) || ctype_digit ( $maxLength )) && ( int ) $maxLength > 0) {
+			$this->maxLength = $maxLength;
+		} else {
+			throw new Exception ( 'max control value length specified was not a valid integer value' );
+		}
+		
+		return $this;
+	}
+	
+	/**
+	 * set the method for this control
+	 * 
+	 * @param string $method
+	 * @return XeroPoint_Control_Abstract
+	 */
+	public function setMethod($method) {
+		$this->method = $method;
+		return $this;
+	}
+	
+	/**
+	 * tells this control to output a label if there is one
+	 * 
+	 * @return XeroPoint_Control_Abstract
+	 */
+	public function showLabel() {
+		$this->displayLabel = true;
+		return $this;
+	}
+}
